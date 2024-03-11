4138946af6f043457da1558e45d81608f5f8f002
hajikazuo <hajikazuo@outlook.com>
2024-03-11 15:40:29 -0300

    fix crud finalizado

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -9,7 +9,9 @@ import { FiUserX } from "react-icons/fi";
 
 export default function Alunos() {
 
-    const [nome, setNome] = useState('');
+    const [searchInput, setSearchInput] = useState('');
+    const [filtro, setFiltro] = useState([]);
+
     const [alunos, setAlunos] = useState([]);
 
     const email = localStorage.getItem('email');
@@ -23,21 +25,55 @@ export default function Alunos() {
         }
     }
 
+    const searchAlunos = (searchValue) => {
+        setSearchInput(searchValue);
+        if (searchInput !== '') {
+            const dadosFiltrados = alunos.filter((item) => {
+                return Object.values(item).join('').toLowerCase()
+                    .includes(searchInput.toLowerCase())
+            });
+            setFiltro(dadosFiltrados);
+        }
+        else {
+            setFiltro(alunos);
+        }
+    }
+
     useEffect(() => {
         api.get('api/alunos', authorization).then(
             response => {
                 setAlunos(response.data);
             }, token)
     })
-    
-    async function logout(){
+
+    async function logout() {
         try {
             localStorage.clear();
-            localStorage.setItem('token','');
-            authorization.headers ='';
+            localStorage.setItem('token', '');
+            authorization.headers = '';
             history('/');
         } catch (error) {
             alert('Não foi possivel fazer o logout' + error)
+        }
+    }
+
+    async function editAluno(id) {
+        try {
+            history(`/aluno/novo/${id}`)
+        } catch (error) {
+            alert('Não foi possivel editar o aluno')
+        }
+    }
+
+    async function deleteAluno(id){
+        try {
+            if(window.confirm('Deseja deletar o aluno de id = ' + id + ' ?'))
+            {
+                await api.delete(`api/alunos/${id}`, authorization);
+                setAlunos(alunos.filter(aluno => aluno.id !== id));
+            }          
+        } catch (error) {
+            alert('Não foi possivel excluir o aluno')
         }
     }
 
@@ -53,30 +89,48 @@ export default function Alunos() {
             </header>
 
             <form>
-                <input type='text' placeholder="Nome" />
-                <button type="button" class='button'>
-                    Filtrar aluno por nome
-                </button>
+                <input type='text' placeholder="filtrar por nome..."
+                    onChange={(e) => searchAlunos(e.target.value)}
+                />
             </form>
             <h1>Relações de alunos</h1>
+            {searchInput.length > 1 ? (
+                <ul>
+                    {filtro.map(aluno => (
+                        <li key={aluno.id}>
+                            <b>Nome: </b>{aluno.nome}<br /><br />
+                            <b>Email: </b>{aluno.email}<br /><br />
+                            <b>Idade: </b>{aluno.idade}<br /><br />
 
-            <ul>
-                {alunos.map(aluno => (
-                    <li key={aluno.id}>
-                        <b>Nome: </b>{aluno.nome}<br /><br />
-                        <b>Email: </b>{aluno.email}<br /><br />
-                        <b>Idade: </b>{aluno.idade}<br /><br />
+                            <button onClick={() => editAluno(aluno.id)} type="button">
+                                <FiEdit size={35} color="#17202a" />
+                            </button>
 
-                        <button type="button">
-                            <FiEdit size={35} color="#17202a" />
-                        </button>
+                            <button type="button" onClick= {()=> deleteAluno(aluno.id)}>
+                                <FiUserX size={35} color="#17202a" />
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <ul>
+                    {alunos.map(aluno => (
+                        <li key={aluno.id}>
+                            <b>Nome: </b>{aluno.nome}<br /><br />
+                            <b>Email: </b>{aluno.email}<br /><br />
+                            <b>Idade: </b>{aluno.idade}<br /><br />
 
-                        <button type="button">
-                            <FiUserX size={35} color="#17202a" />
-                        </button>
-                    </li>
-                ))}
-            </ul>
+                            <button onClick={() => editAluno(aluno.id)} type="button">
+                                <FiEdit size={35} color="#17202a" />
+                            </button>
+
+                            <button type="button" onClick= {()=> deleteAluno(aluno.id)}>
+                                <FiUserX size={35} color="#17202a" />
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
--- src/pages/NovoAluno/index.js
+++ src/pages/NovoAluno/index.js
@@ -1,32 +1,105 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { FiArrowLeft } from "react-icons/fi";
+import api from '../../services/api'
 
 import novoAluno from '../../assets/AddUser.png'
 
 export default function NovoAluno() {
 
     const [id, setId] = useState(null);
-    const {alunoId} = useParams();
-    
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [idade, setIdade] = useState(0);
+
+    const { alunoId } = useParams();
+    const history = useNavigate();
+
+    const token = localStorage.getItem('token');
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    useEffect(() => {
+        if (alunoId === '0')
+            return;
+        else
+            loadAluno();
+    }, [alunoId]);
+
+    async function loadAluno() {
+        try {
+
+            const response = await api.get(`api/alunos/${alunoId}`, authorization);
+
+            setId(response.data.id);
+            setNome(response.data.nome);
+            setEmail(response.data.email);
+            setIdade(response.data.idade);
+
+
+        } catch (error) {
+            alert('Erro ao recuperar o aluno ' + error);
+            history('/alunos');
+        }
+    }
+
+    async function saveOrUpdate(event) {
+        event.preventDefault();
+        
+        const data = {
+            nome, 
+            email,
+            idade
+        }
+
+        try {
+            if(alunoId ==='0')
+            {
+                await api.post('api/alunos',data,authorization);
+            }
+            else{
+                data.id = id;
+                await api.put(`api/alunos/${id}`,data,authorization)
+            }
+
+        } catch (error) {
+            alert('Erro ao gravar aluno ' + error)
+        }
+        history('/alunos');
+    }
+
     return (
         <div className="novo-aluno-container">
-            <div className="content">  
+            <div className="content">
                 <section className="form">
                     <img src={novoAluno} alt="novoAluno"></img>
-                    <h1>{alunoId === '0'? 'Incluir Novo Aluno' : 'Atualizar Aluno'}</h1>
+                    <h1>{alunoId === '0' ? 'Incluir Novo Aluno' : 'Atualizar Aluno'}</h1>
                     <Link className="back-link" to="/alunos">
                         <FiArrowLeft size="15" color="#17202a" />
                         Retornar
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="Nome" />
-                    <input placeholder="Email" />
-                    <input placeholder="Idade" />
-                    <button className="button" type="submit">{alunoId === '0'? 'Incluir' : 'Atualizar'}</button>
+
+                <form onSubmit={saveOrUpdate}>
+                    <input placeholder="Nome"
+                        value={nome}
+                        onChange={e => setNome(e.target.value)}
+                    />
+                    <input placeholder="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input placeholder="Idade"
+                        value={idade}
+                        onChange={e => setIdade(e.target.value)}
+                    />
+                    <button className="button" type="submit">{alunoId === '0' ? 'Incluir' : 'Atualizar'}</button>
                 </form>
+
             </div>
         </div>
     )
